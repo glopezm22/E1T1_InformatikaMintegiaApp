d9ffe5d54138a0bd526f021782c7e07d341e5380
Gorka Lopez <glopezm22@fpsanturtzilh.eus>
2025-10-27 13:16:48 +0100

    Login conectado con el backend y vistas actualizadas. Perfil actualizado para mostrar los datos desde el backend.

--- public/views/perfil.php
+++ public/views/perfil.php
@@ -1,45 +1,51 @@
 <?php require_once "partials/header.html" ?>
 
-<main class="container-fluid text-white vh-100 d-flex align-items-center justify-content-center p-5 container-home">
-    <div class="container mb-5">
-        <div class="row justify-content-center">
-            <div class="col-lg-8 col-md-10">
-                <div class="card shadow-lg rounded-3 border-0 overflow-hidden container-carta">
-                    <div class="card-body p-4 p-md-5 bg-white"> <div class="row">
-                            <div class="col-md-4 text-center mb-4 mb-md-0 border-end pe-4 nombre-perfil">
-                                <img src="../assets/img/perfil/perfil.png" alt="Foto de Perfil" class="rounded-circle mb-3 border border-4 border-secondary">
-                                <h3 class="fw-bold mb-1 text-uppercase">PRUEBA</h3>
-                                <p class="text-primary fw-medium small text-muted">Administratzailea (Admin)</p>
-                            </div>
-                            
-                            <div class="col-md-8 ps-md-5">
-                                <h4 class="mb-4 text-secondary border-bottom pb-2 fw-semibold">Informazio Pertsonala</h4>
-                                
-                                <ul class="list-unstyled">
-                                    <li class="mb-3">
-                                        <p class="mb-0 text-muted small"><i class="bi bi-envelope me-2"></i> Email-a:</p>
-                                        <p class="fw-normal fs-6 text-dark ms-3">a@a.com</p>
-                                    </li>
-                                    <li class="mb-3">
-                                        <p class="mb-0 text-muted small"><i class="bi bi-person me-2"></i> Izena:</p>
-                                        <p class="fw-normal fs-6 text-dark ms-3">prueba</p>
-                                    </li>
-                                    <li class="mb-3">
-                                        <p class="mb-0 text-muted small"><i class="bi bi-person-fill me-2"></i> Abizena:</p>
-                                        <p class="fw-normal fs-6 text-dark ms-3">prueba</p>
-                                    </li>
-                                </ul>
+    <main class="container-fluid text-white vh-100 d-flex align-items-center justify-content-center p-5 container-home">
+        <div class="container mb-5">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-md-10">
+                    <div class="card shadow-lg rounded-3 border-0 overflow-hidden container-carta">
+                        <div class="card-body p-4 p-md-5 bg-white">
+                            <div class="row">
+                                <div class="col-md-4 text-center mb-4 mb-md-0 border-end pe-4 nombre-perfil">
+                                    <img src="../assets/img/perfil/perfil.png" alt="Foto de Perfil" class="rounded-circle mb-3 border border-4 border-secondary">
+                                    <h3 id="erabiltzailea" class="fw-bold mb-1 text-uppercase"></h3>
+                                    <p class="text-primary fw-medium small text-muted" id="rola"></p>
+                                </div>
 
-                                <hr class="mt-4 mb-3">
-                                
-                                <a href="/logout.php" class="text-decoration-none btn btn-danger fw-medium d-inline-block align-items-center"><i class="fas fa-sign-out-alt me-1"></i> Saioa Itxi</a>
+                                <div class="col-md-8 ps-md-5">
+                                    <h4 class="mb-4 text-secondary border-bottom pb-2 fw-semibold">Informazio Pertsonala</h4>
+
+                                    <ul class="list-unstyled">
+                                        <li class="mb-3">
+                                            <p class="mb-0 text-muted small"><i class="bi bi-envelope me-2"></i> NAN:</p>
+                                            <p id="nan" class="fw-normal fs-6 text-dark ms-3"></p>
+                                        </li>
+                                        <li class="mb-3">
+                                            <p class="mb-0 text-muted small"><i class="bi bi-person me-2"></i> Izena:</p>
+                                            <p id="izena" class="fw-normal fs-6 text-dark ms-3"></p>
+                                        </li>
+                                        <li class="mb-3">
+                                            <p class="mb-0 text-muted small"><i class="bi bi-person-fill me-2"></i> Abizena:</p>
+                                            <p id="abizena" class="fw-normal fs-6 text-dark ms-3"></p>
+                                        </li>
+                                    </ul>
+
+                                    <hr class="mt-4 mb-3">
+
+                                    <button id="logoutBtn" class="text-decoration-none btn btn-danger fw-medium d-inline-block align-items-center">
+                                        <i class="fas fa-sign-out-alt me-1"></i> Saioa Itxi
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    </div>
                 </div>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
 
 <?php require_once "partials/footer.html" ?>
+
+<script src="../assets/vendor/bootstrap.min.js"></script>
+<script src="../assets/js/profileLoad.js"></script>
