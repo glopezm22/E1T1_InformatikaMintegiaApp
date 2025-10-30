de226ad3856f62a4fd37574a5888403967baa859
ainara <adiazm24@fpsanturtzhilh.eus>
2025-10-30 12:44:49 +0100

    Sortuta etiketak view .php eta .js

---
+++ public/assets/js/etiketak.js
@@ -0,0 +1,22 @@
+import ekipoakService from './services/ekipoakService.js'
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const produktuak = await ekipoakService.getAll();
+  const select1 = document.querySelector('#select1');
+
+ select1.innerHTML = '<option selected>Hautatu ekipoa</option>';
+  produktuak.forEach(p => {
+    const option = document.createElement('option');
+    option.value = p.id;
+    option.textContent = p.izena;
+    select1.appendChild(option);
+  });
+
+  select1.addEventListener('change', async (e) => {
+    const id = e.target.value;
+    if (id && id !== "Hautatu ekipoa") {
+      const produktua = await inbentarioaService.getById(id);
+      console.log("Produktu hautatua:", produktua);
+    }
+  });
+});
---
+++ public/views/etiketak.php
@@ -0,0 +1,45 @@
+<?php require_once "partials/header.html" ?>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<div class="contenedor my-5">
+    <div class="contenedorEtiketa">
+        <h1 class="mb-5">Etiketak</h1>
+        <form class="p-4 border rounded-3 shadow-sm bg-light">
+            <h2 class="mb-5">Etiketa berria</h2>
+            <div class="mb-3">
+                <label for="select1" class="form-label fw-bold">Hautatu ekipoa</label>
+                <select id="select1" class="form-select">
+                    <option selected>Hautatu ekipoa</option>
+                </select>
+            </div>
+            <div class="mb-3 d-flex align-items-center gap-2">
+                <div class="flex-grow-1">
+                    <label for="select2" class="form-label fw-bold">Kokapena</label>
+                    <select id="select2" class="form-select">
+                        <option selected>Kokapena</option>
+                    </select>
+                </div>
+                <button type="button" class="btn btn-outline-primary mt-4" title="Kokapen berria">
+                    <i class="fa-solid fa-plus"></i> 
+                </button>
+            </div>
+            <div class="d-grid">
+                <button type="submit" class="btn btn-primary">
+                    <i class="fa-solid"></i> Etiketa sortu
+                </button>
+            </div>
+        </form>
+    </div>
+
+    </form>
+</div>
+
+
+
+
+
+</div>
+<script type="module" src="../assets/js/etiketak.js"></script>
+<?php require_once "partials/footer.html" ?>
