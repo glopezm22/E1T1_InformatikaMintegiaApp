4eae94b57df0595e884b68464aadb38618800174
asier <apenao24@fpsanturtzilh.eus>
2025-10-31 09:53:24 +0100

    Aldatzen Ekipoak Background

--- public/views/ekipoak.php
+++ public/views/ekipoak.php
@@ -4,33 +4,33 @@ $CURRENT_USER = require_auth_view('login');
 ?>
 <?php require_once "partials/header.html" ?>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-  integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
-<div class="contenedor my-5">
-  <h1 class="mb-5">Ekipoak</h1>
+<main class="container-fluid text-white d-block align-items-center justify-content-center p-3 p-md-5 container-home">
+  <div class="container bg-light p-5 rounded-2">
+    <div class="contenedor my-5">
+      <h1 class="mb-5 text-dark">Ekipoak</h1>
+      <div style="overflow-y: auto; max-height: 500px;">
+        <table class=" table  table-hover" id="tabla-ekipoak">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Izena</th>
+              <th scope="col">Deskribapena</th>
+              <th scope="col">Marka</th>
+              <th scope="col">Modeloa</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Kategoria ID</th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
 
-
-
-  <table class=" table  table-hover" id="tabla-ekipoak">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Izena</th>
-        <th scope="col">Deskribapena</th>
-        <th scope="col">Marka</th>
-        <th scope="col">Modeloa</th>
-        <th scope="col">Stock</th>
-        <th scope="col">Kategoria ID</th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-      </tr>
-    </thead>
-    <tbody>
-
-    </tbody>
-  </table>
-
-</div>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</main>
 <script type="module" src="../assets/js/ekipoak.js"></script>
 <?php require_once "partials/footer.html" ?>
 
@@ -45,11 +45,6 @@ $CURRENT_USER = require_auth_view('login');
 
       <div class="modal-body">
         ...
-      </div>
-
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
--- public/views/inbentarioa.php
+++ public/views/inbentarioa.php
@@ -4,8 +4,6 @@ $CURRENT_USER = require_auth_view('login');
 ?>
 <?php require_once "partials/header.html" ?>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-  integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
 <main class="container-fluid text-white d-block align-items-center justify-content-center p-3 p-md-5 container-home">
   <div class="container bg-light p-5 rounded-2">
     <div class="contenedor my-5">
@@ -22,7 +20,7 @@ $CURRENT_USER = require_auth_view('login');
                     </tr>
                 </thead>
                 <tbody>
-                    </tbody>
+                </tbody>
             </table>
         </div>
     </div>
@@ -41,11 +39,6 @@ $CURRENT_USER = require_auth_view('login');
 
       <div class="modal-body">
         ...
-      </div>
-
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
