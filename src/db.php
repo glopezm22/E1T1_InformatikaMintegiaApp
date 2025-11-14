663f73227986101796d551ceb9c71cd3eb9c0e62
Gorka Lopez <glopezm22@fpsanturtzilh.eus>
2025-11-14 14:14:55 +0100

    DB modifikatu headerrak gehitzeko eta secrets modifikatuta rutaz.

--- src/db.php
+++ src/db.php
@@ -1,5 +1,14 @@
 <?php
 
+header("Access-Control-Allow-Origin: http://talde1.edu");
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 class DB {
     private $konexioa;
     private $user;
