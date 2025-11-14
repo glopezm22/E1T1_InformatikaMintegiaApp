d2f524150ebd8094f54d4cb2548c600f2370663d
Gorka Lopez <glopezm22@fpsanturtzilh.eus>
2025-11-14 10:56:01 +0100

    Modifikatuta ruten trim-a eta htaccess

--- public/views/routes.php
+++ public/views/routes.php
@@ -9,9 +9,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // "Base"-a detektatu (app dagoen azpikarpetan) eta kendu bideetatik.
 // Horrela / edo /app/ bezalakoetan funtzionatzen du
-$base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
-if ($base === '/') $base = '';
-$route = trim(preg_replace('#^' . preg_quote($base) . '#', '', $uri), '/');
+$route = trim($uri, '/');
 
 // Biderik ez badago -> landing page
 if ($route === '') {
