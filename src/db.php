fb8740ed6e09812fcc7686cdb633c1bafe905ab1
Gorka Lopez <glopezm22@fpsanturtzilh.eus>
2025-11-17 10:33:12 +0100

    https jarrita headerretan

--- src/db.php
+++ src/db.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://talde1.edu");
+header("Access-Control-Allow-Origin: https://talde1.edu");
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
