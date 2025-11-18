945f878f2c7d3b03f800cebcb5cb74ef3e3ae8ad
Gorka Lopez <glopezm22@fpsanturtzilh.eus>
2025-11-18 23:52:00 +0100

    HEADER ALLOW METHODS

--- src/db.php
+++ src/db.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: https://app.talde1.edu");
 header("Access-Control-Allow-Credentials: true");
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
